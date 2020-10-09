417a9e4d2ed1ddcec2932b5da7da5867c1f4e873
JKamue <mail@jkamue.de>
2020-10-09 22:05:48 +0200

    Imported my BufferedScreenController class

--- SatWatcher/Screen/BufferedScreenController.cs
+++ SatWatcher/Screen/BufferedScreenController.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SatWatcher.Screen
 {
     class BufferedScreenController
     {
+        private readonly Panel _panel;
+        private readonly Timer _timer;
+        private readonly Color _color;
+
+        private BufferedGraphicsContext _context;
+        private BufferedGraphics _graphicsBuffer;
+        private Graphics _panelGraphics;
+
+        public readonly List<IScreenObject> _panelObjects = new List<IScreenObject>();
+
+        public BufferedScreenController(Panel panel, Color color)
+        {
+            _panel = panel;
+            _color = color;
+
+            SetupGraphics();
+
+            // Setup Timer
+            _timer = new Timer();
+            _timer.Interval = 1000 / 60;
+            _timer.Tick += Redraw;
+            _timer.Start();
+
+            _panel.SizeChanged += PanelResizeEvent;
+        }
+
+        public void PanelResizeEvent(object sender, EventArgs e) => SetupGraphics();
+
+        public void SetupGraphics()
+        {
+            _context = BufferedGraphicsManager.Current;
+            _panelGraphics = _panel.CreateGraphics();
+            _graphicsBuffer = _context.Allocate(_panelGraphics, _panel.DisplayRectangle);
+        }
+
+        public void Redraw(object sender, EventArgs e)
+        {
+            _graphicsBuffer.Graphics.Clear(_color);
+            _panelObjects.ForEach(x => DrawObject(x, _panel.Bounds));
+            _graphicsBuffer.Render(_panelGraphics);
+        }
+
+        private void DrawObject(IScreenObject o, Rectangle panelBounds)
+        {
+            if (!panelBounds.IntersectsWith(o.GetBounds())) return;
+
+            o.Draw(_graphicsBuffer.Graphics);
+        }
     }
 }
