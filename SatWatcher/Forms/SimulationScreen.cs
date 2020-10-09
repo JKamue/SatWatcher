454683796e54898e7a7f4c0125aa1c3afd3f3809
JKamue <mail@jkamue.de>
2020-10-09 22:01:20 +0200

    Set basic project skeleton

--- SatWatcher/Forms/SimulationScreen.cs
+++ SatWatcher/Forms/SimulationScreen.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SatWatcher
+namespace SatWatcher.Forms
 {
-    public partial class Form1 : Form
+    public partial class SimulationScreen : Form
     {
-        public Form1()
+        public SimulationScreen()
         {
             InitializeComponent();
         }
