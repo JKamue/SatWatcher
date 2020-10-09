5a3c85a1323563cdeaf26bfe90685a5d5be34836
JKamue <mail@jkamue.de>
2020-10-09 22:33:25 +0200

    Added CoordinateCalculator to project coordinates onto the screen

--- SatWatcher/Calculators/CoordinateCalculator.cs
+++ SatWatcher/Calculators/CoordinateCalculator.cs
@@ -8,5 +8,22 @@ namespace SatWatcher.Calculators
 {
     class CoordinateCalculator
     {
+        private readonly Size _size;
+
+        public CoordinateCalculator(Size size)
+        {
+            _size = size;
+        }
+
+        public PointF MapPoint(PointF p) => new PointF(MapLatitude(p.X), MapLongitude(p.Y));
+
+        public float MapLongitude(float lng) => Map(lng, 90, -90, 0, _size.Height);
+
+        public float MapLatitude(float lat) => Map(lat, -180, 180, 0, _size.Width);
+
+        public static float Map(float value, float fromSource, float toSource, float fromTarget, float toTarget)
+        {
+            return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+        }
     }
 }
