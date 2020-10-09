5527a18a7a3b1e8fce5f24afa7e9aa255971b5ab
JKamue <mail@jkamue.de>
2020-10-09 22:28:56 +0200

    Added SatelliteController Logic and events

--- SatWatcher/Satellites/SatellitesController.cs
+++ SatWatcher/Satellites/SatellitesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,27 @@ namespace SatWatcher.Satellites
 {
     class SatellitesController
     {
+        private List<Satellite> _selectedSatellites = new List<Satellite>();
+
+        public event EventHandler SelectionChanged;
+
+        public void SelectSatellite(Satellite sat)
+        {
+            _selectedSatellites.Add(sat);
+            OnSelectionChanged(EventArgs.Empty);
+        }
+
+        public void RemoveSatellite(Satellite sat)
+        {
+            _selectedSatellites.RemoveAll(s => s.ID == sat.ID);
+            OnSelectionChanged(EventArgs.Empty);
+        }
+
+        public IImmutableList<Satellite> SelectedSatellites => _selectedSatellites.ToImmutableList();
+
+        protected virtual void OnSelectionChanged(EventArgs e)
+        {
+            SelectionChanged?.Invoke(this, e);
+        }
     }
 }
