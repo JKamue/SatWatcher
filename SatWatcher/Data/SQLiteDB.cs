a3a90dac15aca4b2c4d99f146c69a1a4ccd1eff2
JKamue <mail@jkamue.de>
2020-10-10 20:28:00 +0200

    Added data structure

---
+++ SatWatcher/Data/SQLiteDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SatWatcher.Satellites;
+
+namespace SatWatcher.Data
+{
+    class SQLiteDB
+    {
+        public List<Satellite> GetAllSatellites()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSatellite(Satellite sat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSatellite(Satellite sat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SatWatcher/Data/TLEApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatWatcher.Data
+{
+    class TLEApi
+    {
+        public static TleLines GetCurrentTleData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    struct TleLines
+    {
+        public readonly string Line1;
+        public readonly string Line2;
+
+        public TleLines(string tleLine1, string tleLine2)
+        {
+            Line1 = tleLine1;
+            Line2 = tleLine2;
+        }
+    }
+}
