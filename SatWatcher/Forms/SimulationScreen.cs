2e3e9390480aab651a2dcc5d91d28f5c7d5dd42a
JKamue <mail@jkamue.de>
2020-10-10 21:50:21 +0200

    SatWatcher now loads satellites from DB and updates them automatically

--- SatWatcher/Forms/SimulationScreen.cs
+++ SatWatcher/Forms/SimulationScreen.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SatWatcher.Data;
 using SatWatcher.Satellites;
 using SatWatcher.Screen;
 
@@ -20,17 +21,20 @@ namespace SatWatcher.Forms
         {
             InitializeComponent();
             var satController = new SatellitesController();
-            var iss = new Satellite(25544, "ISS (ZARYA)",
-                "1 25544U 98067A 20282.49977912 .00004092 00000-0 81550-4 0 9990",
-                "2 25544 51.6442 147.8049 0001578 5.7979 62.1290 15.49295258249583", "SSTV", "145.800");
-            var meteorM2 = new Satellite(40069, "METEOR-M 2",
-                "1 40069U 14037A 20282.44112405 -.00000029 00000-0 58769-5 0 9991",
-                "2 40069 98.4930 315.6513 0004974 325.2073 34.8784 14.20675771324254", "LRPT", "137.100");
+            var database = new SqLiteDb();
 
+            foreach (var sat in database.GetAllSatellites())
+            {
+                var newTles = TLEApi.GetCurrentTleData(sat);
+                database.UpdateTle(newTles);
+            }
 
-            satController.SelectSatellite(iss);
-            satController.SelectSatellite(meteorM2);
-            satController.FocusSatellite(iss);
+            var sats = database.GetAllSatellites();
+            foreach (var sat in sats)
+                satController.SelectSatellite(sat);
+
+            satController.FocusSatellite(sats[0]);
+
             Controller = new BufferedScreenController(pnlSimulation, satController);
         }
     }
